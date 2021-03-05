c2c391c5dc40ff871b5999dbb15503afbe2ba692
Kirill Elizarov <kiril.elizarov@gmail.com>
2021-03-05 17:00:22 +0100

    add Main convert service implementation

---
+++ RWS.Data/Services/MainService.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using RWS.Data.Abstractions;
+using RWS.Data.DataSources;
+using RWS.Data.Models;
+using RWS.Data.Serializers;
+
+namespace RWS.Data.Services
+{
+	public class MainService : IMainService
+	{
+		private readonly IFactory<DataSerializerType, IDataSerialize> _dataSerialzeFactory;
+		private readonly IFactory<DataSourceType, IDataSource> _dataSourceFactory;
+
+		public MainService(IFactory<DataSerializerType, IDataSerialize> dataSerialzeFactory, IFactory<DataSourceType, IDataSource> dataSourceFactory)
+		{
+			_dataSerialzeFactory = dataSerialzeFactory;
+			_dataSourceFactory = dataSourceFactory;
+		}
+
+		public void Convert()
+		{
+			
+			var inputType = DataSerializerType.Xml;
+			var outputType = DataSerializerType.Json;
+			var inputDataSource = DataSourceType.FileSystem;
+			var outputDataSource = DataSourceType.AzureStorage;
+
+			IDataSource inputReader = _dataSourceFactory.Create(inputDataSource);
+			string data = inputReader.GetData();
+			
+			var document = _dataSerialzeFactory.Create(inputType).Deserialize<Document>(data ?? "");
+			string outputString = _dataSerialzeFactory.Create(outputType).Serialize(document);
+			
+			IDataSource outputWriter = _dataSourceFactory.Create(outputDataSource);
+			outputWriter.WriteData(outputString);
+		}
+	}
+}
